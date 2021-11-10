b09c1237afe4ebc0f748cc42d98bb81055caa22f
Валентин <123qwemmm@mail.ru>
2021-11-10 17:50:58 +0300

    всякие доработки ишо

---
+++ bin/load_txt_to_csv/get_txt_from_posts.py
@@ -0,0 +1,24 @@
+from bin.rw.get_image import image_get
+from bin.utils.free_ocr import free_ocr
+
+
+def get_txt_from_posts(posts):
+    texts = []
+    count_pictures = 0
+    for i in posts:
+        if 'text' in i and i['text']:
+            texts.append(i['text'])
+        if 'attachments' in i and i['attachments'][0]['type'] == 'photo':
+            for x in i['attachments'][0]['photo']['sizes']:
+                height = 0
+                url = ''
+                if x['height'] > height:
+                    height = x['height']
+                    url = x['url']
+            if image_get(url, 'image'):
+                a = free_ocr('image')
+                count_pictures += 1
+                print('Картинок распознано - ', count_pictures)
+                if a:
+                    texts.append(a)
+    return texts
