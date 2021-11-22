8c170414a231f6eba852c1117122de6b35561b08
Валентин <123qwemmm@mail.ru>
2021-11-22 19:02:33 +0300

    экспериментирую с чисткой текста

--- bin/sort/clean_text.py
+++ bin/sort/clean_text.py
@@ -8,15 +8,12 @@ ma = pymorphy2.MorphAnalyzer()
 
 def clean_text(text):
     d = str(text)
-    d = re.sub("[^\w]", " ", d)
-    d = re.sub("_", " ", d)
     d = d.lower()
+    d = re.sub(r'\W_', " ", d)
     d = " ".join(ma.parse(unicode(word))[0].normal_form for word in d.split())
-    d = re.sub("\bанон\b", " ", d)
+    d = re.sub(r"\bанон|анон\b|\bанан", " ", d)
     d = ' '.join(word for word in d.split() if len(word) > 3)
     d = list(set(d.split()))
     d = ' '.join(str(e) for e in d)
-    if not d or d == '' or d == ' ' or d == '  ' or d in 'ананимноанонимно':
-        d = ''
 
     return d
